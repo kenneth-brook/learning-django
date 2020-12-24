906df78bdef22165bc5f8f55af76f7d73c2f5bde
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-12-23 19:50:21 -0500

    adding static pages

--- hello/views.py
+++ hello/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
